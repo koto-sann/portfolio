6212de9b37ce1c85385f73a9809aab7e13fe80c6
koto-sann <n.ko5to18@gmail.com>
2025-08-05 23:39:54 +0900

    portfolioをアップロード

---
+++ portfolio/script.js
@@ -0,0 +1,37 @@
+// カルーセル
+   $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+   });
+
+// ローディング
+   window.addEventListener("load", function () {
+   // ローディングを非表示にしてメインを表示
+   const loading = document.getElementById("loading");
+   const content = document.getElementById("after-loading");
+
+   this.setTimeout(function () {
+   loading.style.display = "none";
+   content.style.display = "block";
+   }, 1000);
+});
+
+// TOPに戻るボタン
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100) {
+            $('.page-top').fadeIn(300);
+        } else {
+            $('.page-top').fadeOut(300);
+        }
+    });
+    
+    $(function() {
+        $('.page-top').click(function() {
+            $('html,body').animate({ scrollTop: 0 }, 400);
+        });
+    });
+
+
